aadfa628043422f45b6d60eb32ecf0accfdc884b
fati <etsukazu200@gmail.com>
2021-11-24 23:41:26 +0100

    initialiser projet spring

---
+++ src/main/java/com/docmananger/DocumentManangerApplication.java
@@ -0,0 +1,13 @@
+package com.docmananger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentManangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentManangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docmananger/DocumentManangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docmananger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentManangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
